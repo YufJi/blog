90b5cba40577f67546b26556e0984252f72c9d88
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-27 11:10:18 +0000

    Deploy YufJi/blog to github.com/YufJi/blog.git:gh-pages

---
+++ service-worker.js
@@ -0,0 +1,225 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "fda3af21e87fb33d915595a073548060"
+  },
+  {
+    "url": "assets/css/0.styles.97723510.css",
+    "revision": "73381b39b70976880c830582c3ea6537"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/10.e460dd9c.js",
+    "revision": "745215568b2013bff2d3425cc974670e"
+  },
+  {
+    "url": "assets/js/11.09a513e1.js",
+    "revision": "522f7af9a04266b78f18bcf6b7954f1a"
+  },
+  {
+    "url": "assets/js/12.66c19975.js",
+    "revision": "046018b8cc3443c8c9ba4dd743f76c6d"
+  },
+  {
+    "url": "assets/js/13.9f159cd7.js",
+    "revision": "0b1086257a077619b88e5d9ae9fd3f7e"
+  },
+  {
+    "url": "assets/js/14.b1df2884.js",
+    "revision": "d04d7f1aeafc5be9fca5acd52a1dfc7b"
+  },
+  {
+    "url": "assets/js/15.514cef4c.js",
+    "revision": "1018d5d2b0a1870df606588ac62efb7b"
+  },
+  {
+    "url": "assets/js/16.1c9348ef.js",
+    "revision": "029454cb5c91bd6199369a8d69fb94f3"
+  },
+  {
+    "url": "assets/js/17.3a83a8f7.js",
+    "revision": "edf42cb04faeb548d65d6b1320097a1d"
+  },
+  {
+    "url": "assets/js/18.7d775f30.js",
+    "revision": "c93e1e0077b34a676878658e2fcc6b0e"
+  },
+  {
+    "url": "assets/js/19.4fb66411.js",
+    "revision": "6204c6dd0087cb8dd3f0b17c4f932735"
+  },
+  {
+    "url": "assets/js/2.ca7eb0ac.js",
+    "revision": "72d50edf78f6fe9d5f7e51bc6d2359f8"
+  },
+  {
+    "url": "assets/js/20.a6df5ce1.js",
+    "revision": "827aafaf2912287e39a0c5ed0e6fe55a"
+  },
+  {
+    "url": "assets/js/21.518d7292.js",
+    "revision": "79a9ac0f2c8b01ad7ce02d77d448dfd1"
+  },
+  {
+    "url": "assets/js/22.fdc54c63.js",
+    "revision": "f1341f17d2383578f49828b46666f5e8"
+  },
+  {
+    "url": "assets/js/23.8ad4ff2e.js",
+    "revision": "5be25b659a6e7718c0ff3e388f24d13f"
+  },
+  {
+    "url": "assets/js/24.9f8ff4e9.js",
+    "revision": "7028d2c1074524327662fc8ed0d9f13b"
+  },
+  {
+    "url": "assets/js/3.a7c5f8f2.js",
+    "revision": "d3e358516cae7b5a33e9271c149eab0e"
+  },
+  {
+    "url": "assets/js/4.7cbaf6ab.js",
+    "revision": "48f129e2ca69ca57272469e3e9cf477f"
+  },
+  {
+    "url": "assets/js/5.9fc44ebe.js",
+    "revision": "e8013577baf32b21c1fbf9d2298d6ce9"
+  },
+  {
+    "url": "assets/js/6.51bd86b7.js",
+    "revision": "728aac0f86706b172abdf41133c44702"
+  },
+  {
+    "url": "assets/js/7.4b7a6ff9.js",
+    "revision": "0bb2e54157cc573a571e45127be6743d"
+  },
+  {
+    "url": "assets/js/8.d91730d2.js",
+    "revision": "eec1aa01d881add4bf187a7bdb9cfea1"
+  },
+  {
+    "url": "assets/js/9.e91dacf6.js",
+    "revision": "5df806cf4d4483976470ec855ea0dcbf"
+  },
+  {
+    "url": "assets/js/app.74ea537a.js",
+    "revision": "d3954c4ed1b42355fc915928047b3ef8"
+  },
+  {
+    "url": "easy/binarySearch.html",
+    "revision": "53181f13091aec09a3baa91902e6af0e"
+  },
+  {
+    "url": "easy/distributeCandies.html",
+    "revision": "a136363d308bf5ea545a068a13835462"
+  },
+  {
+    "url": "easy/findContentChildren.html",
+    "revision": "41ad9b0d02565c2e5631286c01d0cfbc"
+  },
+  {
+    "url": "easy/isSymmetric.html",
+    "revision": "37d293e24c115833402a3cc35a493a43"
+  },
+  {
+    "url": "easy/reverse.html",
+    "revision": "61484a97b67f5fea6d79278ee1e272dc"
+  },
+  {
+    "url": "easy/searchInsert.html",
+    "revision": "138fabf63d43785a819adb703019637c"
+  },
+  {
+    "url": "easy/twoSum.html",
+    "revision": "c4738dd903d24248e810d360763e45ba"
+  },
+  {
+    "url": "hard/findMedianSortedArrays.html",
+    "revision": "68509741392f5cd450dd10dcb52d63a9"
+  },
+  {
+    "url": "hard/firstMissingPositive.html",
+    "revision": "0b26acc56832b2eabe6baf6962851d41"
+  },
+  {
+    "url": "hard/minWindow.html",
+    "revision": "daa49af89490fee7002d0823d22ebf45"
+  },
+  {
+    "url": "index.html",
+    "revision": "4331b8bf34ed8dd2ecd29c7c9a7503ef"
+  },
+  {
+    "url": "middle/addTwoNumbers.html",
+    "revision": "ff814608ba1c2470ba00f1f733d2bb8e"
+  },
+  {
+    "url": "middle/combine.html",
+    "revision": "fd022a10a19b1a324109ab38ca9b42ed"
+  },
+  {
+    "url": "middle/findMinArrowShots.html",
+    "revision": "cdc462679510f5b51793726a87b56639"
+  },
+  {
+    "url": "middle/generateParenthesis.html",
+    "revision": "7f6cb8114078c0bde1ccfcf82aa9a84d"
+  },
+  {
+    "url": "middle/lengthOfLongestSubstring.html",
+    "revision": "6da71bc9d2b61efa5ea4d7b912db7981"
+  },
+  {
+    "url": "middle/numFactoredBinaryTrees.html",
+    "revision": "d49e87a03179c8fa5601afa6d520c757"
+  },
+  {
+    "url": "middle/pow.html",
+    "revision": "30edd0ba08b6ecf0c37c432181db0341"
+  },
+  {
+    "url": "middle/threeSum.html",
+    "revision": "6ba6ed17765bba2a788b5b5b5c72e1ef"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
