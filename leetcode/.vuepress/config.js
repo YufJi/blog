fd39c693b25f2b7c80abef042ab6076d7f1cf055
ji_yf <ji_yf@ctrip.com>
2019-12-30 11:04:04 +0800

    feat: 🎸 pwa

--- leetcode/.vuepress/config.js
+++ leetcode/.vuepress/config.js
@@ -37,14 +37,14 @@ module.exports = {
     ],
   },
   head: [
-    ['link', { rel: 'icon', href: '/icons/logo.jpeg' }],
+    ['link', { rel: 'icon', href: '/icons/icon.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'no' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/logo.jpeg' }],
-    ['link', { rel: 'mask-icon', href: '/icons/logo.jpeg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/logo.jpeg' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/icon.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/icon.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: ['@vuepress/pwa', {
