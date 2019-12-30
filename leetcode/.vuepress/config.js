da155e3e440844f18701a9e4085d1d1dd9a02f50
ji_yf <ji_yf@ctrip.com>
2019-12-30 10:22:53 +0800

    feat: 🎸 pwa

--- leetcode/.vuepress/config.js
+++ leetcode/.vuepress/config.js
@@ -36,6 +36,17 @@ module.exports = {
       }
     ],
   },
+  head: [
+    ['link', { rel: 'icon', href: '/icons/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/logo.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/logo.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/logo.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   plugins: ['@vuepress/pwa', {
     serviceWorker: true,
     updatePopup: {
