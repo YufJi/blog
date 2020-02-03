fc7003bfc3dd257669a469d1d35030f484faa70d
jyf <ji_yf@Ctrip.com>
2020-02-04 01:00:26 +0800

    docs: ✏️ docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -66,10 +66,9 @@ module.exports = {
     ['link', { rel: 'icon', href: '/icons/icon.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#F3F3F3' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'no' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
     ['link', { rel: 'apple-touch-icon', href: '/icons/icon.png' }],
-    ['link', { rel: 'mask-icon', href: 'https://h5.sinaimg.cn/m/weibo-lite/mask-icon.svg', color: '#F3F3F3' }],
     ['meta', { name: 'msapplication-TileImage', content: '/icons/icon.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
